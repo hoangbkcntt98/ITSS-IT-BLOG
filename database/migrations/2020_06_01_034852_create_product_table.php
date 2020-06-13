83e163c0d0fc36b52474766ad7f41a745ec1186d
hoangbkcntt98 <20166135@student.hust.edu.vn>
2020-06-13 08:36:48 +0700

    migrate database

---
+++ database/migrations/2020_06_01_034852_create_product_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('image');
+            $table->string('CPU');
+            $table->string('RAM');
+            $table->string('disk');
+            $table->string('graphic_card');
+            $table->string('OS');
+            $table->string('size');
+            $table->string('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
