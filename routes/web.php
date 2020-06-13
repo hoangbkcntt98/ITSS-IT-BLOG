ccdfb0fbe74d454b316152dfd5d0ac85de24288f
hoangbkcntt98 <20166135@sis.hust.edu.vn>
2020-06-13 11:42:33 +0700

    add layout and refactoring view structer

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
+    return view('layouts.home');
+})->name('/');
+Route::resource('user','ProfileController');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
