875cc7426751c1a567257f6f00cecb26d3561bdc
hoangbkcntt98 <20166135@sis.hust.edu.vn>
2020-06-14 18:56:05 +0700

    update sidebar

--- resources/views/layouts/elements/sidebar.blade.php
+++ resources/views/layouts/elements/sidebar.blade.php
@@ -4,20 +4,20 @@
 							<div class="panel panel-default">
 								<div class="panel-heading">
 									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordian" href="#sportswear">
+										<a data-toggle="collapse" data-parent="#accordian" href="#laptop">
 											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
-											Sportswear
+											LAPTOP
 										</a>
 									</h4>
 								</div>
-								<div id="sportswear" class="panel-collapse collapse">
+								<div id="laptop" class="panel-collapse collapse">
 									<div class="panel-body">
 										<ul>
-											<li><a href="#">Nike </a></li>
-											<li><a href="#">Under Armour </a></li>
-											<li><a href="#">Adidas </a></li>
-											<li><a href="#">Puma</a></li>
-											<li><a href="#">ASICS </a></li>
+											<li><a href="#">DELL </a></li>
+											<li><a href="#">ASUS </a></li>
+											<li><a href="#">APPLE </a></li>
+											<li><a href="#">HP</a></li>
+											<li><a href="#">LENOVO </a></li>
 										</ul>
 									</div>
 								</div>
@@ -25,84 +25,43 @@
 							<div class="panel panel-default">
 								<div class="panel-heading">
 									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordian" href="#mens">
+										<a data-toggle="collapse" data-parent="#accordian" href="#tablet">
 											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
-											Mens
+											TABLET
 										</a>
 									</h4>
 								</div>
-								<div id="mens" class="panel-collapse collapse">
+								<div id="tablet" class="panel-collapse collapse">
 									<div class="panel-body">
 										<ul>
-											<li><a href="#">Fendi</a></li>
-											<li><a href="#">Guess</a></li>
-											<li><a href="#">Valentino</a></li>
-											<li><a href="#">Dior</a></li>
-											<li><a href="#">Versace</a></li>
-											<li><a href="#">Armani</a></li>
-											<li><a href="#">Prada</a></li>
-											<li><a href="#">Dolce and Gabbana</a></li>
-											<li><a href="#">Chanel</a></li>
-											<li><a href="#">Gucci</a></li>
+											<li><a href="#">DELL </a></li>
+											<li><a href="#">ASUS </a></li>
+											<li><a href="#">APPLE </a></li>
+											<li><a href="#">HP</a></li>
+											<li><a href="#">LENOVO </a></li>
 										</ul>
 									</div>
 								</div>
 							</div>
-							
 							<div class="panel panel-default">
 								<div class="panel-heading">
 									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordian" href="#womens">
+										<a data-toggle="collapse" data-parent="#accordian" href="#pc">
 											<span class="badge pull-right"><i class="fa fa-plus"></i></span>
-											Womens
+											PC
 										</a>
 									</h4>
 								</div>
-								<div id="womens" class="panel-collapse collapse">
+								<div id="pc" class="panel-collapse collapse">
 									<div class="panel-body">
 										<ul>
-											<li><a href="#">Fendi</a></li>
-											<li><a href="#">Guess</a></li>
-											<li><a href="#">Valentino</a></li>
-											<li><a href="#">Dior</a></li>
-											<li><a href="#">Versace</a></li>
+											<li><a href="#">DELL </a></li>
+											<li><a href="#">ASUS </a></li>
+											<li><a href="#">APPLE </a></li>
+											<li><a href="#">HP</a></li>
+											<li><a href="#">LENOVO </a></li>
 										</ul>
 									</div>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Kids</a></h4>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Fashion</a></h4>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Households</a></h4>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Interiors</a></h4>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Clothing</a></h4>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Bags</a></h4>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Shoes</a></h4>
 								</div>
 							</div>
 						</div><!--/category-products-->
@@ -111,13 +70,11 @@
 							<h2>Brands</h2>
 							<div class="brands-name">
 								<ul class="nav nav-pills nav-stacked">
-									<li><a href="#"> <span class="pull-right">(50)</span>Acne</a></li>
-									<li><a href="#"> <span class="pull-right">(56)</span>Grüne Erde</a></li>
-									<li><a href="#"> <span class="pull-right">(27)</span>Albiro</a></li>
-									<li><a href="#"> <span class="pull-right">(32)</span>Ronhill</a></li>
-									<li><a href="#"> <span class="pull-right">(5)</span>Oddmolly</a></li>
-									<li><a href="#"> <span class="pull-right">(9)</span>Boudestijn</a></li>
-									<li><a href="#"> <span class="pull-right">(4)</span>Rösch creative culture</a></li>
+									<li><a href="#"> <span class="pull-right">(50)</span>DELL</a></li>
+									<li><a href="#"> <span class="pull-right">(56)</span>ASUS</a></li>
+									<li><a href="#"> <span class="pull-right">(27)</span>APPLE</a></li>
+									<li><a href="#"> <span class="pull-right">(32)</span>LENOVO</a></li>
+									<li><a href="#"> <span class="pull-right">(5)</span>HP</a></li>
 								</ul>
 							</div>
 						</div><!--/brands_products-->
@@ -128,7 +85,7 @@
 								 <input type="text" class="span2" value="" data-slider-min="0" data-slider-max="600" data-slider-step="5" data-slider-value="[250,450]" id="sl2" ><br />
 								 <b class="pull-left">$ 0</b> <b class="pull-right">$ 600</b>
 							</div>
-						</div><!--/price-range-->
+						</div>/price-range
 						
 						<div class="shipping text-center"><!--shipping-->
 							<img src="images/home/shipping.jpg" alt="" />
