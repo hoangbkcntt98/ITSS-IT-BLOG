0564ccac3f56fbd56cbbd21c3b3a90eb48dbcc68
hoangbkcntt98 <20166135@sis.hust.edu.vn>
2020-06-14 18:45:16 +0700

    add brand and category to product

--- database/migrations/2020_06_14_104858_add_brand__to_product.php
+++ database/migrations/2020_06_14_104858_add_brand__to_product.php
@@ -14,6 +14,7 @@ class AddBrandToProduct extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
+
             $table->string('brand_id')->references('id')->on('brand')->onDelete('cascade');
             $table->string('category_id')->references('id')->on('category')->onDelete('cascade');
         });
