8efc6022967f1c36ea1fd2666e906dd2d9289591
HikPb <p.ngocba@gmail.com>
2020-06-15 10:24:30 +0700

    fix database table

--- database/migrations/2020_06_01_035211_create_comment_table.php
+++ database/migrations/2020_06_01_035211_create_comment_table.php
@@ -13,7 +13,7 @@ class CreateCommentTable extends Migration
      */
     public function up()
     {
-        Schema::create('comment', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->unsignedInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
@@ -32,6 +32,6 @@ class CreateCommentTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comment');
+        Schema::dropIfExists('comments');
     }
 }
