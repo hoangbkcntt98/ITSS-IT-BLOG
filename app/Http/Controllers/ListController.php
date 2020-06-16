1c35c697f560c0271bab075a86cfe385b5747ab9
HikPb <p.ngocba@gmail.com>
2020-06-16 07:01:37 +0700

    filter fix

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -17,12 +17,34 @@ class ListController extends Controller
 
     public function filter(Request $request)
     {
-        $product = Product::query()
-            ->brand_id($request)
-            ->cpu($request)
-            ->ram($request)
-            ->disk($request)
-            ->size($request);
+        // $product = Product::query()
+        //     ->brand_id($request)
+        //     ->cpu($request)
+        //     ->ram($request)
+        //     ->disk($request)
+        //     ->size($request);
+        $brand_id = $request->input('brand_id');
+        $cpu= $request->input('cpu');
+        $ram= $request->input('ram');
+        $disk= $request->input('disk');
+        $size= $request->input('size');
+        $product = Product::query();
+        if($brand_id!=null){
+            $product = $product->brand_id($request);
+        }
+        if($cpu!=null){
+            $product = $product->cpu($request);
+        }
+        if($ram!=null){
+            $product = $product->ram($request);
+        }
+        if($disk!=null){
+            $product = $product->disk($request);
+        }
+        if($size!=null){
+            $product = $product->size($request);
+        }
+        
         $products =  $product->paginate(12);
 
         return view('list.index', ['products' => $products]);
--- resources/views/layouts/elements/filterbar.blade.php
+++ resources/views/layouts/elements/filterbar.blade.php
@@ -2,11 +2,11 @@
     <div class="col">
         <label for="brand_id">Hãng:</label>
         <select name="brand_id" id="brand_id">
+            <option hidden selected></option>
             <option value="1">ACER</option>
             <option value="2">APPLE</option>
             <option value="3">ASUS</option>
             <option value="4">DELL</option>
-
             <option value="5">HP</option>
             <option value="6">LENOVO</option>
         </select>
@@ -16,11 +16,11 @@
     <div class="col">
         <label for="cpu">CPU:</label>
         <select name="cpu" id="cpu">
+            <option hidden selected></option>
             <option value="Core i3">Intel Core i3</option>
             <option value="Core i5">Intel Core i5</option>
             <option value="Core i7">Intel Core i7</option>
             <option value="Core i9">Intel Core i9</option>
-
             <option value="Ryzen 3">Ryzen 3</option>
             <option value="Ryzen 5">Ryzen 5</option>
             <option value="Ryzen 7">Ryzen 7</option>
@@ -30,10 +30,10 @@
     <div class="col">
         <label for="ram">RAM:</label>
         <select name="ram" id="ram">
+            <option hidden selected></option>
             <option value="4">4Gb</option>
             <option value="8">8Gb</option>
             <option value="16">16Gb</option>
-
             <option value="32">32Gb</option>
         </select>
     </div>
@@ -41,6 +41,7 @@
     <div class="col">
         <label for="disk">Ổ cứng:</label>
         <select name="disk" id="disk">
+            <option hidden selected></option>
             <option value="128">128Gb</option>
             <option value="256">256Gb</option>
             <option value="512">512Gb</option>
@@ -50,10 +51,10 @@
     <div class="col">
         <label for="size">Màn hình:</label>
         <select  name="size" id="size">
+            <option hidden selected></option>
             <option value="12">Màn 12 Inch</option>
             <option value="13.5">Màn 13.5 Inch</option>
             <option value="14">Màn 14 Inch</option>
-
             <option value="15.6">Màn 15.6 Inch</option>
             <option value="17">Màn 17 Inch</option>
 
