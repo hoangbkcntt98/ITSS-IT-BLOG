f38c6b1744fbf96e30a263c8233028766bb7899d
HikPb <p.ngocba@gmail.com>
2020-06-16 09:01:37 +0700

    fix

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -10,8 +10,8 @@ use Illuminate\Support\Facades\DB;
 class ArticleController extends Controller
 {
     public function index($id){
-        $article = Article::where('id',$id)->first();
-        $author = User::where('id',$article->user_id)->first();
+        $article = DB::table('articles')->where('id',$id)->first();
+        $author = DB::table('users')->where('id',$article->user_id)->first();
         $comments = DB::table('comments')->join('users','comments.user_id','=','users.id')
             ->select('users.name', 'comments.content','comments.published_at')->get();
 
